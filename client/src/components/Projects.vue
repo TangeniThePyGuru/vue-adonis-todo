84efca3f285aa0a7447bddc932ea8cdc85934a75
Tangeni Shikomba <tangenishikomba@gmail.com>
2019-05-11 01:54:29 +0200

    refactoring done

---
+++ client/src/components/Projects.vue
@@ -0,0 +1,13 @@
+<template>
+  <Panel title="Projects"></Panel>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
---
+++ client/src/views/Projects.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="home">
+    <v-container>
+      <v-layout>
+        <v-flex xs4>
+          <Projects></Projects>
+        </v-flex>
+        <v-flex xs8 class="pl-4">
+          <Tasks></Tasks>
+        </v-flex>
+      </v-layout>
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+import Projects from '@/components/Projects.vue';
+import Tasks from '@/components/Tasks.vue';
+
+export default {
+  name: 'projects',
+  components: {
+    Projects,
+    Tasks,
+  },
+
+};
+</script>
